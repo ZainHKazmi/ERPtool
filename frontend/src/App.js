a1eeae15285bf54a0fe6a030468d5b5d52971452
Zain Kazmi <zain.kazmi@mail.utoronto.ca>
2020-01-30 10:40:17 -0500

    Create Endpoint from Client into Django service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 class App extends Component {
 	  state = {
-		      users: []
+		      users: {},
 		    };
 	/* 
 	 *    This is where the magic happens
 	 */
 	 async componentDidMount() {
-		try {
-			            const res = await fetch('http://127.0.0.1:8000/users/');
-			            const users = await res.json();
-			            this.setState({
-					            users
-					          });
-			          } catch (e) {
-					        console.log(e);
-					      }
-		    }
-
-	  render() {
-		      return (
-			            <div>
-			      	     {JSON.stringify(this.state)}
-			              {this.state.users.map(item => (
-					                <div key={item.email}>
-					                  <h1>{item.email}</h1>
-					                  <span>{item.username}</span>
-					                </div>
-					              ))}
-			            </div>
-			          );
+	 	axios.get('http://127.0.0.1:8000/users/').then(res=>{
+			console.log(res.data);
+			this.setState({users: res});
+		});
+	 }
+	 render() {
+		      return(
+			<div>
+			 {JSON.stringify(this.state)}
+			</div>
+	 
+	 	);
 		    }
 }
 
