138f84d3303122e82a00f1adbc8d885c9dccda7f
Zain Kazmi <zain.kazmi@mail.utoronto.ca>
2020-01-30 16:45:46 -0500

    Code styling in Users.js

--- frontend/src/Users.js
+++ frontend/src/Users.js
@@ -7,9 +7,6 @@ class Users extends Component {
 	  state = {
 		      users: {},
 		    };
-	/* 
-	 *    This is where the magic happens
-	 */
 	 async componentDidMount() {
 	 	axios.get('http://127.0.0.1:8000/users/').then(res=>{
 			console.log(res.data);
