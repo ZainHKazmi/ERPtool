3d2da7da21ee1eaa1b304a9ee890be148edca6ee
root <root@LAPTOP-0E77H9QR.localdomain>
2020-02-20 22:54:07 -0500

    Added Login screen to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Switch, BrowserRouter} from 'react-router-dom';
 
 import Users from './Users';
+import Login from './Login';
 
 class App extends Component {
 	render(){
@@ -11,7 +12,8 @@ class App extends Component {
 			<div>
 			<BrowserRouter>
 				<Switch>
-				<Route exact path='/' component={Users}/>
+				<Route exact path='/' component={Login}/>
+				<Route exact path='/AllUsers' component={Users}/>
 				</Switch>
 			</BrowserRouter>
 			</div>
